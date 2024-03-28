6afee9aa9c726187f9d46340c52991ed9ee55906
ritvik <ritvik.juttukonda@mail.utoronto.ca>
2024-03-28 18:46:02 -0400

    next section added

--- RAG.py
+++ RAG.py
@@ -31,3 +31,48 @@ llm = OpenAIEmbedding()
 Settings.llm = llm
 
 Settings.embed_model = embed_model
+
+import json
+from llama_index.core import Document
+from llama_index.core.schema import MetadataMode
+
+# Convert the DataFrame to a JSON string representation
+documents_json = dataset_df.to_json(orient='records')
+
+# Load the JSON string into a python list of dictionaries
+documents_list = json.loads(documents_json)
+
+llama_documents = []
+
+for document in documents_list:
+    # Value for metadata must be one of (str, int, float, None)
+    document["writers"] = json.dumps(document["writers"])
+    document["languages"] = json.dumps(document["languages"])
+    document["genres"] = json.dumps(document["genres"])
+    document["cast"] = json.dumps(document["cast"])
+    document["directors"] = json.dumps(document["directors"])
+    document["countries"] = json.dumps(document["countries"])
+    document["imdb"] = json.dumps(document["imdb"])
+    document["awards"] = json.dumps(document["awards"])
+
+    # Create a Document object with the text and excluded metadata for llm and embedding models
+    llama_document = Document(
+        text=document["fullplot"],
+        metadata=document,
+        excluded_llm_metadata_keys=["fullplot", "metacritic"],
+        excluded_embed_metadata_keys=["fullplot", "metacritic", "poster", "num_mflix_comments", "runtime", "rated"],
+        metadata_template="{key}=>{value}",
+        text_template="Metadata: {metadata_str}\n-----\nContent: {content}",
+        )
+    
+    llama_documents.append(llama_document)
+
+# Observing an example of what the LLM and Embedding model receive as input
+print(
+    "\nThe LLM sees this: \n",
+    llama_documents[0].get_content(metadata_mode=MetadataMode.LLM),
+)
+print(
+    "\nThe Embedding model sees this: \n",
+    llama_documents[0].get_content(metadata_mode=MetadataMode.EMBED),
+)
